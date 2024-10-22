5feb97fe16c5630664e18d4273e68e76a09dd377
SAI KIRAN <saikiran4824@gmail.com>
2024-10-22 14:03:38 +0530

    Update site.ts

--- config/site.ts
+++ config/site.ts
@@ -1,7 +1,7 @@
 const SiteConfig = {
     "title": "Sai Kiran",
 <<<<<<< HEAD
-    "description": "Programming With Harry is a blog for your programming doubts",
+    "description": "Portfolio and Blogs of Sai Kiran",
 =======
     "description": "Portfolio and Blogs of Sai Kiran",
 >>>>>>> 878e2af62635031e84bba1aecc2d82cbd2ffb5b7
