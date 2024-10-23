cc1cfba90aa31cfe936326fb7d87688516080ba0
SAI KIRAN <saikiran4824@gmail.com>
2024-10-23 11:49:19 +0530

    Resolved merge conflict in site.ts

--- config/site.ts
+++ config/site.ts
@@ -1,12 +1,7 @@
+
 const SiteConfig = {
     "title": "Sai Kiran",
-<<<<<<< HEAD
     "description": "Portfolio and Blogs of Sai Kiran",
-=======
-    "description": "Portfolio and Blogs of Sai Kiran",
->>>>>>> 878e2af62635031e84bba1aecc2d82cbd2ffb5b7
-    
-}
+};
 
-
-export default SiteConfig
+export default SiteConfig;
